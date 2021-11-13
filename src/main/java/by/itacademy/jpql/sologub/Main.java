e61f5a3da2b4d53773776d7fb3c6e1cd916483be
nikita <nikitasologub42@gmail.com>
2021-11-13 18:13:54 +0300

    entity manager helper added
    maven dependencies fixed and added
    hibernate config xml fixed

--- src/main/java/by/itacademy/jpql/sologub/Main.java
+++ src/main/java/by/itacademy/jpql/sologub/Main.java
@@ -1,7 +1,20 @@
 package by.itacademy.jpql.sologub;
 
+import by.itacademy.jpql.sologub.dao.EntityManagerHelper;
+import by.itacademy.jpql.sologub.model.Cat;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 public class Main {
     public static void main(String[] args) {
+        EntityManager manager = EntityManagerHelper.getInstance().getEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
 
+        transaction.begin();
+        Cat someCat = manager.find(Cat.class, 1);
+        System.out.println("Котик " + someCat);
+        transaction.commit();
+        manager.close();
     }
 }
